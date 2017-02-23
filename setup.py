1c61131c241a3b716eca7dd878c1bb1cd35737e4
voidnologo <(none)>
2017-02-23 15:56:34 -0600

    setup for 0.0.3

--- setup.py
+++ setup.py
@@ -8,18 +8,17 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='jpprint',
-    version='0.0.2',
+    version='0.0.3',
     description='Basic print and comparison of two dictionaries and/or json files',
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/voidnologo/jpprint',
     author='voidnologo',
-    author_email='reboot.yourself@gmail.com',
+    author_email='void.no.logo@gmail.com',
     license='None',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
